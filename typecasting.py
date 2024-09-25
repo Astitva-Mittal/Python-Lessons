63c6ec6c54f1beb12025c7707d298ba8219bb83e
Astitva-Mittal <mittalastitva@gmail.com>
2024-09-26 00:07:01 +0530

    typecasting
    learned about typecasting and data type heirarchy

---
+++ typecasting.py
@@ -0,0 +1,21 @@
+# Conversion of one data type to another data type
+# through the use of typecasting functions
+a = "12"
+b = "3"
+print(a+b)  # will print 123
+print(int(a)+int(b))  # will print 15
+# functions work if the data type of the function matches with the data type we're trying to convert
+c = "thor"
+d = 2
+# print(int(c)+int(d))      #will show a value error
+'''this was an example of explicit typecasting'''
+
+# implicit typecasting
+x = 1.2
+y = 2
+# automatically converts y to float. this is done b/c float lies higher in the data type hierarchy within python itself.
+print(x+y)
+z = x+y
+print(type(x))
+print(type(y))
+print(type(z))  # z will be float even though y is int
