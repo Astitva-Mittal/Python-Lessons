540d4a7849ad2f73144ad207eb16a57d443e92e3
Astitva-Mittal <mittalastitva@gmail.com>
2024-09-25 23:42:51 +0530

    Learned about operators

---
+++ calculator.py
@@ -0,0 +1,19 @@
+# basically, its not a calculator since it doesn't really take inputs...
+a = 25
+b = 3
+add = a + b
+sub = a-b
+mul = a*b
+div = a/b
+simpledivide = a//b
+rem = a % b
+pow = a**b
+print("The 2 numbers are", a, "and", b)
+print(a, "+", b, "=", add)
+print(a, "-", b, "=", sub)
+print(a, "x", b, "=", mul)
+print(a, "/", b, "=", div)
+print(a, "/", b, "(in terms of natural numbers) =", simpledivide)
+print("The remainder of", a, "/", b, "=", rem)
+print(a, "to the power of", b, "=", pow)
+print("Thanks")
---
+++ operators.py
@@ -0,0 +1,8 @@
+print(15+6)
+print(15-6)
+print(15*6)
+print(15/6)
+# Floor division operator. run the code to understand with it does...
+print(15//6)
+print(15 % 6)  # Module op. gives remainder..
+print(5**3)  # Exponential op. power written after **
