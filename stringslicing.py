a55b1d40c269e31a962e803c241921ee0f512e07
Astitva-Mittal <mittalastitva@gmail.com>
2024-10-20 15:13:00 +0530

    String slicing

--- stringslicing.py
+++ stringslicing.py
@@ -1,4 +1,11 @@
 string = "Hey Wassup??"
 length = len(string)  # will provide the lenth og the string
 print("The length of the string is", length)
-print("Now printing the initial 6 letters of the string", string[0:5])
+# Including 0 but not 5. so basically 0 to 4
+print("Now printing the initial 5 letters of the string", string[0:5])
+print(string[0:-3])  # here the compiler will print 0 to len-3
+print(string[:5])  # the missing 0 will automatically be understood
+print(string[2:7])
+print(string[-12])  # will print the first letter of the string
+# nm = "Harry"
+# print(nm[-4])
